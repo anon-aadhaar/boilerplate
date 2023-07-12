d406c4314e1c1adbe09154341d4c088c5ea716c1
Yanis Meziane <yanis.meziane@essec.edu>
2023-07-12 14:26:33 +0200

    installed tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
 import { FileInput, pdfUpload } from "country-identity-kit";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,7 +13,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
+      <main className="">
         <h1>Welcome to Country Identity Example</h1>
         <p>Prove your anon addhaar ownership</p>
 
