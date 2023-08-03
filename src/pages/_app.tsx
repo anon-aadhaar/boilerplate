f7006775c2f72ff86f82d9f3a871f5777d65e281
Yanis Meziane <yanis.meziane@essec.edu>
2023-08-03 17:13:03 +0200

    SDK working + UI for proof

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { CountryIdentityProvider } from "country-identity-kit";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <CountryIdentityProvider>
+      <Component {...pageProps} />
+    </CountryIdentityProvider>
+  );
 }
